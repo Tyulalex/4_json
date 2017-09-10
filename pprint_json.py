80d302a8103d6ad57544e830a401a743d59cfdb3
Tatiana Karabut <tyulalex@gmail.com>
2017-09-10 16:02:30 +0300

    add exception handling

--- pprint_json.py
+++ pprint_json.py
@@ -12,5 +12,17 @@ def pretty_print_json(json_content):
 
 
 if __name__ == '__main__':
-    json_content = load_data(filepath=sys.argv[1])
+    raw_file_path = None
+    try:
+        raw_file_path = sys.argv[1]
+        json_content = load_data(filepath=raw_file_path)
+    except IndexError:
+        print("Please enter correct path to the json file. Example of launch python pprint_json.py <Path to json>")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("The provided file {} not found. Please check if this path is correct and retry".format(raw_file_path))
+        sys.exit(1)
+    except json.decoder.JSONDecodeError:
+        print("The provided file {} is not a json file. Please check and retry".format(raw_file_path))
+        sys.exit(1)
     pretty_print_json(json_content)
